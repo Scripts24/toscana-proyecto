c49f0c1f7436e6161d8bf522546efa54a4f1f327
Patricia Atanasoff <system.informatica365@gmail.com>
2023-01-30 02:07:02 -0300

    scroll, navbar activo, cambio img

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,6 @@
+
+/* Activa menú hamburguesa*/
+
 const ham = document.querySelector('.ham');
 const enlaces = document.querySelector('.enlaces-menu');
 const barras = document.querySelectorAll('.ham span');
@@ -6,3 +9,17 @@ ham.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
     barras.forEach(child =>  {child.classList.toggle('animado')});
 });
+
+
+/*Indica página activa*/
+
+const seleccionado = location.href;
+const menu_item = document.querySelectorAll('a');
+const menu_length = menu_item.length;
+
+for (let i = 0; i < menu_length; i++){
+    if(menu_item[i].href === seleccionado){
+        menu_item[i].className = "active"
+    }
+}
+
---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+
+window.onscroll = progress_scroll;
+
+function progress_scroll (){
+    let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (scroll/height) * 100;
+    document.getElementById('barra').style.width = scrolled + '%';
+}
