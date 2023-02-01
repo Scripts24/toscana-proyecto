47ce2ebaa62c57f618124c0cb490b19d78321593
Patricia Atanasoff <system.informatica365@gmail.com>
2023-02-01 04:06:15 -0300

    cambios img, inicio logica carrito

---
+++ js/main.js
@@ -0,0 +1,337 @@
+const productos = [
+    //Pastas
+    {
+        id: "pasta-01",
+        titulo: "Ñoquis",
+        imagen: "../img/noquis.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-02",
+        titulo: "Canelones",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-03",
+        titulo: "Lasaña",
+        imagen: "../img/lasana.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-04",
+        titulo: "Capeletis",
+        imagen: "../img/capeletis.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-05",
+        titulo: "Ravioles",
+        imagen: "../img/ravioles.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-06",
+        titulo: "Sorrentinos",
+        imagen: "../img/sorrentinos.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-07",
+        titulo: "Parpadelle",
+        imagen: "../img/parpadelle.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-08",
+        titulo: "Spaghetti",
+        imagen: "../img/spaghetti.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-09",
+        titulo: "Fetuccini",
+        imagen: "../img/fetuccini.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-10",
+        titulo: "Farfalle",
+        imagen: "../img/farfalle.jpg",
+        categoria: {
+            nombre: "Pastas",
+            id: "pastas"
+        },
+        precio: 1000
+    },
+    //Pizzas
+    {
+        id: "pizza-1",
+        titulo: "Pepperoni",
+        imagen: "../img/pepperoni.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pizza-2",
+        titulo: "Margarita",
+        imagen: "../img/margarita.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pizza-3",
+        titulo: "Napolitana",
+        imagen: "../img/napolitana.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pizza-4",
+        titulo: "Pizza",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pizza-5",
+        titulo: "Pizza",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pizza-6",
+        titulo: "Pizza",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pizza-7",
+        titulo: "Pizza",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pizza-8",
+        titulo: "Pizza",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pizza-9",
+        titulo: "Pizza",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    {
+        id: "pizza-10",
+        titulo: "Pizza",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Pizzas",
+            id: "pizzas"
+        },
+        precio: 1000
+    },
+    //Postres
+    {
+        id: "postre-1",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+    {
+        id: "postre-2",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+    {
+        id: "postre-3",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+    {
+        id: "postre-4",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+    {
+        id: "postre-5",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+    {
+        id: "postre-6",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+    {
+        id: "postre-7",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+    {
+        id: "postre-8",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+    {
+        id: "postre-9",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+    {
+        id: "postre-10",
+        titulo: "Postre",
+        imagen: "../img/canelones.jpg",
+        categoria: {
+            nombre: "Postres",
+            id: "postres"
+        },
+        precio: 1000
+    },
+];
+
+
+const contenedor_productos = document.querySelector("#contenedor-productos");
+
+
+
+
+/*---Función para inyectar productos en el HTML--- */
+/*---Parámetro para que carguen solo los productos de la categoría elegida--- */
+function cargar_productos(productos_elegidos) {
+
+    contenedor_productos.innerHTML = "";/*---Vaciar el contenedor_productos antes de realizar el forEach del array, sino c/ vez que cliqueemos los productos se duplican--- */
+
+    productos_elegidos.forEach(producto => {
+        const div = document.createElement("div");/*---Creamos un div con la class producto--- */
+        div.classList.add("producto");
+        div.innerHTML = `
+                    <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                    <h3 class="producto-titulo">${producto.titulo}</h3>
+                    <p class="producto-precio">$${producto.precio}</p>
+                    <button class="producto-agregar" id= "${producto.id}"> Agregar</button>
+                    `;
+
+        contenedor_productos.append(div);
+
+    })
+
+
+}
+
+/*---Llamado a la función--- */
+cargar_productos(productos);/*---Parámetro para que al cargar por primera vez la página se cargue el array completo--- */
